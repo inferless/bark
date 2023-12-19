13946e35fc958706dd81aaed674117cd737ea26c
ujjawalPeak01 <119571874+ujjawalPeak01@users.noreply.github.com>
2023-12-19 16:07:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+from transformers import AutoProcessor, AutoModel
 
+
+class InferlessPythonModel:
+    def initialize(self):
+        self.processor = AutoProcessor.from_pretrained("suno/bark")
+        self.model = AutoModel.from_pretrained("suno/bark")
+    
+    def infer(self, inputs):
+        prompt = inputs["prompt"]
+        inputs = self.processor(
+            text=[prompt],
+            return_tensors="pt",
+        )
+        speech_values = self.model.generate(**inputs, do_sample=True)
+        audio_numpy = speech_values.cpu().numpy().squeeze()
+
+        buffer = io.BytesIO()
+        write(buffer, self.model.generation_config.sample_rate, audio_numpy)
+        buffer.seek(0)
+
+        base64_audio = base64.b64encode(buffer.read()).decode('utf-8')
+        return {"generated_audio_base64": base64_audio}
+
+    def finalize(self,args):
+        self.pipe = None
