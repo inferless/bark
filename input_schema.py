03146282c608828330b03be36812d67d2c0900b3
rbgo404 <150957746+rbgo404@users.noreply.github.com>
2024-02-07 11:02:30 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,8 @@
+INPUT_SCHEMA = {
+    "prompt": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["Hello, my name is Suno. And, uh — and I like pizza. [laughs] But I also have other interests such as playing tic tac toe."]
+    }
+}
